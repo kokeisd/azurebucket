ac85614b2951f0b5280638e03a07229d3f2c6593
Kelvin Ko <kelvin_mk_ko@manulife.com>
2019-11-11 02:07:42 +0000

    added web inventory

--- azurebucket/vmbucket/filters.py
+++ azurebucket/vmbucket/filters.py
@@ -9,8 +9,23 @@ class VMBucketFilter(FilterSet):
         #fields = {"host": ["exact", "contains"], "userid": ["exact","contains"]}
         #fields = {"name": ["exact", "contains"]}
         #fields = []
-        fields = {"name": ["icontains"], 
-                "subscription": ["icontains"]
+        fields = {
+                "subscription": ["icontains"],
+                "resource_group": ["icontains"], 
+                "name": ["icontains"],
+                "status": ["icontains"],
+                "primary_ip_address": ["icontains"],
+                "secondary_ip_addresses": ["icontains"],
+                "location": ["icontains"],
+                "vm_size": ["icontains"],
+                "os_disk": ["icontains"],
+                "os_disk_size": ["icontains"],
+                "disks": ["icontains"],
+                "avset": ["icontains"],
+                "tags": ["icontains"],
+                "last_update_time": ["icontains"],
+                
+
                 }
         exclude = {"id"}
         type='contains'
