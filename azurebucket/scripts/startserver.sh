5dc79c8149372916b2a970e5d2ace7a7216d10b2
Ko, Kelvin MK <kelvin_mk_ko@manulife.com>
2019-11-13 02:04:17 +0000

    update

--- azurebucket/scripts/startserver.sh
+++ azurebucket/scripts/startserver.sh
@@ -1,2 +1,5 @@
+export PROJECT_PROFILE=dev
+MYNAME=`basename "$0"`
+source /home/mladmin/kelvinko/git-projects/azurebucket/venv/bin/activate
 cd /home/mladmin/kelvinko/git-projects/azurebucket/azurebucket/
-nohup python manage.py runserver 0.0.0.0:8000  2>&1 >> logs/serverlog.`date +%y%m%d%H%M%S` &
+nohup python manage.py runserver 0.0.0.0:8000  2>&1 >> logs/${MYNAME}.`date +%y%m%d%H%M%S` &
