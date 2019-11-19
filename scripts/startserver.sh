ec8aad02c4a8af4023464634acc8a38c327dbd91
Ko, Kelvin MK <kelvin_mk_ko@manulife.com>
2019-11-19 06:40:10 +0000

    restructure the project folders

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -2,4 +2,6 @@ export PROJECT_PROFILE=dev
 MYNAME=`basename "$0"`
 source /home/mladmin/kelvinko/git-projects/azurebucket/venv/bin/activate
 cd /home/mladmin/kelvinko/git-projects/azurebucket/azurebucket/
+
+# worker = (2 x $num_cores) + 1
 nohup python manage.py runserver 0.0.0.0:8000  2>&1 >> logs/${MYNAME}.`date +%y%m%d%H%M%S` &
