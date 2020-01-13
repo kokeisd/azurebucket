0dc60cfe70b140113142de650c539522e104b8a4
vagrant <vagrant@ubuntu18>
2020-01-13 15:50:13 +0000

    fix error

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -4,7 +4,7 @@ export NUMOFWORKERS=`expr $NUMOFCORES \* 2 + 1`
 if [[ "${DJANGO_ENV}" = "DEV" ]] ; then
 export DJANGO_PORT=8000
 export DJANGO_DEBUG=True
-elif [[] "${DJANGO_ENV}" = "PROD" ]] ;then
+elif [[ "${DJANGO_ENV}" = "PROD" ]] ;then
 export DJANGO_PORT=80
 export DJANGO_DEBUG=False
 else
