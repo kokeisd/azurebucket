a44db7a012333e1f47d2c56da1080be519dc69f4
vagrant <vagrant@ubuntu18>
2020-01-13 15:49:17 +0000

    fixed lint error

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,12 +1,10 @@
 export NUMOFCORES=`grep -c ^processor /proc/cpuinfo`
 export NUMOFWORKERS=`expr $NUMOFCORES \* 2 + 1`
 
-if [ $DJANGO_ENV = "DEV" ]
-then
+if [[ "${DJANGO_ENV}" = "DEV" ]] ; then
 export DJANGO_PORT=8000
 export DJANGO_DEBUG=True
-elif [ $DJANGO_ENV = "PROD" ] 
-then
+elif [[] "${DJANGO_ENV}" = "PROD" ]] ;then
 export DJANGO_PORT=80
 export DJANGO_DEBUG=False
 else
@@ -14,8 +12,7 @@ export DJANGO_PORT=8000
 export DJANGO_DEBUG=True
 fi
 
-if [ $DJANGO_API_SERVER != "" ]
-then
+if [[ -z "${DJANGO_API_SERVER}"  ]]; then
 export DJANGO_API_ENDPOINT="http://${DJANGO_API_SERVER}:${DJANGO_PORT}/vmbucket/"
 else
 export DJANGO_API_ENDPOINT="http://localhost:${DJANGO_PORT}/vmbucket/"
