b596e7275aab95bb709917535e707c9807c0f0ff
Ko, Kelvin MK <kelvin_mk_ko@manulife.com>
2020-01-15 09:35:44 +0000

    update multistage build

--- changelog.md
+++ changelog.md
@@ -1,11 +1,14 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
-
-## 0.1.1 2019-12-16
+## 0.1.2 2020-01-14
 ### changed
-- Fixed some dockerfile error and precreate log dir
+- reduced the size of the image
 
-## 0.1.2 2019-11-15
+## 0.1.2 2019-12-16
+### changed
+- Fixed some dockerfile errors
+
+## 0.1.1 2019-11-15
 ### Changed
 - First release
--- scripts/buildimage.sh
+++ scripts/buildimage.sh
@@ -1,4 +1,4 @@
-docker stop azurebucket*
-docker rm azurebucket*
-docker build -t  azurebucket-web:v0.1.1 . -f Dockerfile.web
-docker build -t  azurebucket-cron:v0.1.1 . -f Dockerfile.cron
+docker kill $(docker ps -aqf name='azurebucket')
+docker rm $(docker ps -aqf name='azurebucket')
+docker build -t  azurebucket-web:v0.1.3 . -f Dockerfile.web
+docker build -t  azurebucket-cron:v0.1.3 . -f Dockerfile.cron
